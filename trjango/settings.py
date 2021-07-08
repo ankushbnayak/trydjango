c364f82ef0c9b009cbd448b981abfe24b322ae2a
sansairama <31078691+sansairama@users.noreply.github.com>
2021-07-08 00:47:08 -0700

    debug is true

--- trjango/settings.py
+++ trjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9@u(4%w%b*bht=+q8set@8yxz#q4avn+b@+@)ok8oq0a#n9+_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['varansha.herokuapp.com','127.0.0.1']
 
